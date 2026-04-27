8a9c64d1ce97edb3bc005f3fbef6b9439becaf0c
Victor N <victorkjngo@gmail.com>
2026-04-26 23:14:37 -0700

    Bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // your Tailwind styles
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
