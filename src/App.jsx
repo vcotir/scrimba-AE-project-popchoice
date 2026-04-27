8c09ddb493960fb3ff8677e89941d0045af783e8
Victor N <victorkjngo@gmail.com>
2026-04-26 23:15:45 -0700

    Add scaffold of react hook form :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function App() {
-    const [count, setCount] = useState(0);
+    const { 
+        register, handleSubmit,
+        formState: { errors, isSubmitting }
+    } = useForm()
+
+    const onSubmit = (data) => { 
+        console.log('data', data)
+    }
 
     return (
         <>
-            <h1>Hello world!</h1>
-        </>
-    )
+        <h1>PopChoice</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label>What’s your favorite movie and why?</label>
+                    <input
+                        {
+                        ...register('favoriteMovie', { required: "We'd like to learn about your favorite movie :)"})
+                        }
+                    />
+
+                </div>
+                <button type="submit">Submit</button>
+            <label>Are you in the mood for something new or a classic?</label>
+            <label>Do you wanna have fun or do you want something</label>
+        </form>
+      </>
+    );
 }
