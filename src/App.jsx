e7db20e973253155bdd9e429bcd350447d5a7aad
Victor N <victorkjngo@gmail.com>
2026-04-29 00:50:40 -0700

    Add initial input fields to app component

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,49 @@ export default function App() {
 
     const onSubmit = (data) => { 
         console.log('data', data)
+        fetch('http://localhost:3000/recommendation', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
     }
 
     return (
-        <>
+      <>
         <h1>PopChoice</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label>What’s your favorite movie and why?</label>
-                    <input
-                        {
-                        ...register('favoriteMovie', { required: "We'd like to learn about your favorite movie :)"})
-                        }
-                    />
+          <div>
+            <label>What’s your favorite movie and why?</label>
+            <input
+              {...register("favorite_movie", {
+                  required: "We'd like to learn about your favorite movie :)",
+                  value: "Inception"
+              })}
+            />
+          </div>
 
-                </div>
-                <button type="submit">Submit</button>
+          <div>
             <label>Are you in the mood for something new or a classic?</label>
+            <input
+              {...register("newness", {
+                  required: "We'd like to learn about your favorite movie :)",
+                  value: "New all the way!"
+              })}
+            />
+          </div>
+
+          <div>
             <label>Do you wanna have fun or do you want something</label>
+            <input
+              {...register("fun_or_serious", {
+                  required: "We'd like to learn about your favorite movie :)",
+                  value: "A generous splash of fun please!"
+              })}
+            />
+          </div>
+          <button type="submit">Submit</button>
         </form>
       </>
     );
