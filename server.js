b2e646d21747eb009ea65c47a35ac280c16c45e2
Victor N <victorkjngo@gmail.com>
2026-04-29 00:49:56 -0700

    Add CORS middleware Add recommendations endpoint

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { serve } from "@hono/node-server";
+
+import { openai, supabase } from "./config.js";
 
 const app = new Hono();
 
+app.use(
+  cors({
+    origin: "*",
+    allowedMethods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: "Content-Type",
+  }),
+);
 app.get("/", (c) => c.text("Hono!"));
+
+app.post("/recommendation", async (c) => {
+  const body = await c.req.json();
+  console.log("body", body);
+
+  await main(body)
+  // return something here
+  return true
+});
 
 serve(app, (info) => {
   console.log(`Listening on http://localhost:${info.port}`); // Listening on http://localhost:3000
